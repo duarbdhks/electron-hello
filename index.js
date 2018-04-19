50ba241b22be33865a5690d5dc0b0c962675aaff
duarbdhks <duarbdhks@gmail.com>
2018-04-19 15:15:01 +0900

    Add Source files

---
+++ index.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+function onClosed() {
+    mainWindow = null;
+}
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 600,
+        height: 480
+    });
+
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+    mainWindow.on('closed', onClosed);
+}
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if(!mainWindow){
+        createWindow();
+    }
+});
+
+app.on('ready', () => {
+    createWindow();
+});
